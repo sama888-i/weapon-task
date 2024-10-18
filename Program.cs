e26ebc655c2615075b698a7454faaad420930083
sama888-i <samaki-bp215@code.edu.az>
2024-10-19 00:19:24 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon weapon = new Weapon(99, 54, "tekli");
+            bool f = false;
+            string operation;
+            do
+            {
+                Console.WriteLine("Emeliyyat secin,0- Informasiya almaq ucun, 1- Shoot metodu ucun, 2- GetRemainBulletCount  metodu ucun, 3- Reload metodu ucun, 4-ChangeFireMode metodu ucun, 5- Proqrami dayandirmaq ucun ");
+                operation = Console.ReadLine();
+                switch (operation)
+                {
+                    case "0":
+                        weapon.GetInfo();
+                        break;
+                    case "1":
+                        weapon.Shoot();
+                        break;
+                    case "2":
+                        weapon.GetRemainBulletCount();
+                        break;
+                    case "3":
+                        weapon.Reload();
+                        break;
+                    case "4":
+                        weapon.ChangeFireMode();
+                        break;
+                    case "5":
+                        f = true;
+                        break;
+                    default:
+                        Console.WriteLine("Duzgun secim edin");
+                        break;
+
+
+                }
+
+            } while (!f);
+        }
+    }
+}
---
+++ Weapon.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    public class Weapon
+    {
+        int _capacity;
+        int _count;
+       public string mode;
+
+        public Weapon(int capacity, int count, string mode)
+        {
+            Capacity = capacity;
+            Count = count;
+            Mode = mode;
+
+        }
+        public int Capacity
+        {
+            get
+            {
+                return _capacity;
+
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _capacity = value;
+                }
+                else
+                {
+                    Console.WriteLine("0 dan boyuk deyer verin");
+                }
+
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+            set
+            { if( value> Capacity)
+                {
+                    Console.WriteLine("Sayi duzgun verin");
+                }
+                else
+                {
+                    _count = value;
+                }
+
+            }
+        }
+        public string Mode
+        {
+            get
+            {
+                return mode;
+            }
+            set
+            {
+                if (value == "tekli" || value == "auto")
+                {
+                    mode = value;
+                }
+                else
+                {
+                    Console.WriteLine("bele mod yoxdur");
+                }
+
+            }
+        }
+        public void Shoot()
+        {if(Count == 0)
+            {
+                Console.WriteLine("gulle yoxdur");
+                return;
+            }
+        if (mode == "tekli")
+            {
+                Count--;
+                Console.WriteLine("Atis edildi");
+                Console.WriteLine("Qalan gulle sayi:" + Count);
+            }
+        else if(mode == "auto")
+            {
+                Count = 0;
+                Console.WriteLine("Atis edildi");
+                Console.WriteLine("Qalan gulle sayi:" + Count);
+            }
+
+
+        }
+        public void GetRemainBulletCount()
+        {
+            Console.WriteLine("Daragin dolmasi ucun lazim olan gulle sayi:" + (Capacity - Count));       
+        }
+        public void Reload()
+        {    
+            if (Count < Capacity)
+            {
+                
+             int load = Capacity - Count;
+             Count += load ;
+              Console.WriteLine("Daraqdaki Gulle sayi yenilendi.Gulle sayi: " + Count);
+            }
+        else if( Count == Capacity)
+            {
+                Console.WriteLine("Daraq zaten dolu ");
+                return;
+            }
+
+        }
+        public void ChangeFireMode()
+        { if(mode == "tekli")
+            {
+                mode = "auto";
+                Console.WriteLine("Atis modu tekliden avtomatike kecirildi");
+            }
+        else if(mode == "auto")
+            {
+                mode = "tekli";
+                Console.WriteLine("Atis modu avtomatikden tekliye kecirildi");
+            }
+
+        }
+        public void ChangeCapasity(int newCapacity)
+        {
+            Capacity = newCapacity;
+            Console.WriteLine("Daragin yeni tutumu:" + Capacity);
+
+
+
+        }
+        public void ChangeCount(int newCount)
+        {
+            if(Count< newCount || newCount == 0)
+            {
+                Console.WriteLine("yeni daxil etdiyiniz say tutumdan boyuk ve ya 0 -a beraber ola bilmez");
+            }
+            else
+            {
+                Count = newCount;
+                Console.WriteLine("Daragin yeni gulle sayi:" + Count);
+            }
+
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"Gulle tutumu:{Capacity},Gulle sayi:{Count},Atis modu:{mode}");
+        }
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+    }
+}
